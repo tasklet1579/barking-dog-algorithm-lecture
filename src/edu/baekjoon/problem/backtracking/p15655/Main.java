b69926642b1914856b3f4ffd429f7447cbb7c538
tasklet1579 <tasklet1579@gmail.com>
2022-05-11 09:30:36 +0900

    N과 M (6) 문제풀이

---
+++ src/edu/baekjoon/problem/backtracking/p15655/Main.java
@@ -0,0 +1,67 @@
+package edu.baekjoon.problem.backtracking.p15655;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+
+    private static final String DELIMITER = " ";
+    private static final String NEW_LINE = "\n";
+
+    private static int N;
+    private static int M;
+    private static int[] numbers;
+    private static int[] answer;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer info = new StringTokenizer(readString(), DELIMITER);
+        N = readInt(info.nextToken());
+        M = readInt(info.nextToken());
+        numbers = new int[N];
+        answer = new int[M];
+
+        numbers();
+
+        Arrays.sort(numbers);
+
+        sequence(0, 0);
+
+        System.out.println(sb);
+    }
+
+    public static int readInt(String text) {
+        return Integer.parseInt(text);
+    }
+
+    public static String readString() throws IOException {
+        return br.readLine();
+    }
+
+    public static void numbers() throws IOException {
+        StringTokenizer num = new StringTokenizer(readString(), DELIMITER);
+        for (int idx = 0; idx < N; idx++) {
+            numbers[idx] = readInt(num.nextToken());
+        }
+    }
+
+    public static void sequence(int depth, int loc) {
+        if (depth == M) {
+            for (int num : answer) {
+                sb.append(num).append(DELIMITER);
+            }
+            sb.append(NEW_LINE);
+            return;
+        }
+
+        for (int idx = loc; idx < N; idx++) {
+            answer[depth] = numbers[idx];
+            sequence(depth + 1, idx + 1);
+        }
+    }
+}
